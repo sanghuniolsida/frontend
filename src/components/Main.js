029c03087dad3ad5d78256e60c7b5cfcfba6b672
[nakarac] <[wnsgkr1208@naver.com]>
2025-05-14 16:37:27 +0900

    question이랑 creating v2

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,12 @@ import Header from './Header';
 import Footer from './Footer';
 import './Main.css';
 
-function Main({ children }) {
+function Main({ children, hideFooter }) {
   return (
     <div className="app-wrapper">
       <Header />
       <main className="main-scroll">{children}</main>
-      <Footer />
+      {!hideFooter && <Footer />}
     </div>
   );
 }
--- src/pages/Creating/index.js
+++ src/pages/Creating/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import Main from '../../components/Main';
+import Header from '../../components/Header';
+import '../../components/Main.css';
 import './Creating.css';
 
 function CreatingPage() {
   return (
-    <Main>
-      <div className="creating-page">
-        <div className="creating-content">
-          <div className="icon-circle">
-            <img
-              src="/Creating.png"
-              alt="동화 제작 중 아이콘"
-              className="creating-icon"
-            />
-          </div>
-          <div className="creating-text">
-            동화 제작중
-            <span className="dot dot1">.</span>
-            <span className="dot dot2">.</span>
-            <span className="dot dot3">.</span>
+    <div className="app-wrapper">
+      <Header />
+      <main className="main-scroll">
+        <div className="creating-page">
+          <div className="creating-content">
+            <div className="icon-circle">
+              <img
+                src="/Creating.png"
+                alt="동화 제작 중 아이콘"
+                className="creating-icon"
+              />
+            </div>
+            <div className="creating-text">
+              동화 제작중
+              <span className="dot dot1">.</span>
+              <span className="dot dot2">.</span>
+              <span className="dot dot3">.</span>
+            </div>
           </div>
         </div>
-      </div>
-    </Main>
+      </main>
+    </div>
   );
 }
 
