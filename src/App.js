930d617deda8ef698227bdf5d97102bc016cd92f
sanghun98 <p0526000@gmail.com>
2025-06-01 01:47:47 +0900

    중복 삭제

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import QuestionSectionPage from './pages/QuestionSection';
 import CreatingPage from './pages/Creating';
 import Library from './pages/Library';
 import StoryProcess from './pages/StoryProcess';
-import GrowthPage from './pages/GrowthPage';
 import StatsPage from './pages/StatsPage';
-import StoryBookPage from './pages/StoryBookPage';
+
 
 function App() {
   return (
@@ -23,9 +22,7 @@ function App() {
         <Route path="/creating" element={<CreatingPage />} />
         <Route path="/library" element={<Library />} />
         <Route path="/storyprocess" element={<StoryProcess />} />
-        <Route path="/growth" element={<GrowthPage />} />
         <Route path="/stats" element={<StatsPage />} />
-        <Route path="/storybook" element={<StoryBookPage />} />
         {/* 다른 페이지들도 여기 추가 */}
       </Routes>
     </Router>
