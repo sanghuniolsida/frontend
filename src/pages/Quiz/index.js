c2aff08f0695d07126e7604ee990d35dfa4bc9c0
sanghun98 <p0526000@gmail.com>
2025-06-11 14:05:21 +0900

    feat: 동화 추가 생성

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -22,7 +22,7 @@ function Quiz() {
       ],
       questions: [
         {
-          question: 'Q1. 퍼피와 키티가 처음 놀이터에서 만나서 한 행동은 무엇이었나요?', //이해력
+          question: '퀴즈1. 퍼피와 키티가 처음 놀이터에서 만나서 한 행동은 무엇이었나요?', //이해력
           category: '이해력',
           options: [
             '서로를 보고 도망쳤어요',
@@ -33,7 +33,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q2. 퍼피와 키티가 싸우다가 어떤 일이 일어났나요?', //기억력
+          question: '퀴즈2. 퍼피와 키티가 싸우다가 어떤 일이 일어났나요?', //기억력
           category: '기억력',
           options: [
             '퍼피와 키티가 울기 시작했어요',
@@ -44,7 +44,7 @@ function Quiz() {
           answerIndex: 2
         },
         {
-          question: 'Q3. 퍼피는 어떻게 자신이 멋지다고 보여주려고 했나요?', //주의 집중
+          question: '퀴즈3. 퍼피는 어떻게 자신이 멋지다고 보여주려고 했나요?', //주의 집중
           category: '주의 집중력',
           options: [
             '크게 짖었어요',
@@ -55,7 +55,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q4. 퍼피와 키티가 마지막에 어떻게 되었나요?', //언어추론
+          question: '퀴즈4. 퍼피와 키티가 마지막에 어떻게 되었나요?', //언어추론
           category: '언어 추론력',
           options: [
             '끝까지 말도 안 하고 헤어졌어요',
@@ -80,7 +80,7 @@ function Quiz() {
       ],
       questions: [
         {
-          question: 'Q1. 이 동화에서 루루는 어떤 행동으로 구름을 도와줬나요?', //이해력
+          question: '퀴즈1. 이 동화에서 루루는 어떤 행동으로 구름을 도와줬나요?', //이해력
           category: '이해력',
           options: [
             '구름을 혼냈어요',
@@ -91,7 +91,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q2. 구름이 웃은 뒤 어떤 일이 일어났나요?', //기억력
+          question: '퀴즈2. 구름이 웃은 뒤 어떤 일이 일어났나요?', //기억력
           category: '기억력',
           options: [
             '비가 내렸어요',
@@ -102,7 +102,7 @@ function Quiz() {
           answerIndex: 2
         },
         {
-          question: 'Q3. 루루는 어떤 색 망토를 두르고 있었나요?', //주의 집중
+          question: '퀴즈3. 루루는 어떤 색 망토를 두르고 있었나요?', //주의 집중
           category: '주의 집중력',
           options: [
             '파란색',
@@ -113,7 +113,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q4. 구름이 처음에 슬펐던 이유는 무엇인가요?', //언어추론
+          question: '퀴즈4. 구름이 처음에 슬펐던 이유는 무엇인가요?', //언어추론
           category: '언어 추론력',
           options: [
             '바람이 불어서요',
@@ -138,7 +138,7 @@ function Quiz() {
       ],
       questions: [
         {
-          question: 'Q1. 이 이야기에서 무무는 왜 친구에게 미안한 마음이 들었을까요?', //이해력
+          question: '퀴즈1. 이 이야기에서 무무는 왜 친구에게 미안한 마음이 들었을까요?', //이해력
           category: '이해력',
           options: [
             '삽을 잃어버려서요',
@@ -149,7 +149,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q2. 무무는 무엇을 하다가 화가 났나요?', //기억력
+          question: '퀴즈2. 무무는 무엇을 하다가 화가 났나요?', //기억력
           category: '기억력',
           options: [
             '밥을 먹다가요',
@@ -160,7 +160,7 @@ function Quiz() {
           answerIndex: 2
         },
         {
-          question: 'Q3. 무무에게 먼저 다가가 말을 건 친구의 이름은 누구였나요?', //주의 집중
+          question: '퀴즈3. 무무에게 먼저 다가가 말을 건 친구의 이름은 누구였나요?', //주의 집중
           category: '주의 집중력',
           options: [
             '레이',
@@ -171,7 +171,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q4. 무무는 마지막에 어떤 마음이 되었나요?', //언어추론
+          question: '퀴즈4. 무무는 마지막에 어떤 마음이 되었나요?', //언어추론
           category: '언어 추론력',
           options: [
             '더 화가 났어요',
@@ -196,7 +196,7 @@ function Quiz() {
       ],
       questions: [
         {
-          question: 'Q1. 달록은 어떤 친구를 만나 함께 길을 걸었나요?', //이해력
+          question: '퀴즈1. 달록은 어떤 친구를 만나 함께 길을 걸었나요?', //이해력
           category: '이해력',
           options: [
             '토끼',
@@ -207,7 +207,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q2. 새는 왜 덤불 속에 숨어 있었나요?', //기억력
+          question: '퀴즈2. 새는 왜 덤불 속에 숨어 있었나요?', //기억력
           category: '기억력',
           options: [
             '달록을 피하려고',
@@ -215,10 +215,10 @@ function Quiz() {
             '날개가 아파서', //✅
             '잠을 자려고'
           ],
-          answerIndex: 1
+          answerIndex: 2
         },
         {
-          question: 'Q3. 달록과 새가 마주했을 때, 새의 날개에는 무엇이 있었나요?', //주의 집중
+          question: '퀴즈3. 달록과 새가 마주했을 때, 새의 날개에는 무엇이 있었나요?', //주의 집중
           category: '주의 집중력',
           options: [
             '붕대',
@@ -229,7 +229,7 @@ function Quiz() {
           answerIndex: 1
         },
         {
-          question: 'Q4. 마지막에 달록과 새가 함께 웃었던 이유는 무엇일까요?', //언어추론
+          question: '퀴즈4. 마지막에 달록과 새가 함께 웃었던 이유는 무엇일까요?', //언어추론
           category: '언어 추론력',
           options: [
             '숲에서 맛있는 걸 찾았기 때문에',
@@ -241,6 +241,180 @@ function Quiz() {
         }
       ]
     },
+    '친구가 필요해': {
+      images: [
+        '/friendbear/friednbear1.png',
+        '/friendbear/friednbear2.png',
+        '/friendbear/friednbear3.png',
+        '/friendbear/friednbear4.png',
+        '/friendbear/friednbear5.png',
+        '/friendbear/friednbear6.png',
+        '/friendbear/friednbear7.png',
+        '/friendbear/friednbear8.png',
+      ],
+      questions: [
+        {
+          question: '퀴즈1. 북극곰이 친구들에게 다가가지 못했던 이유는 무엇이었나요?', //이해력
+          category: '이해력',
+          options: [
+            '곰이 너무 멀리 살았기 때문이에요.',
+            '친구들이 곰을 무서워해서 도망쳤기 때문이에요.', //✅
+            '친구들과 싸웠기 때문이에요.',
+            '곰에게는 상처가 있었기 때문이에요.'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈2. 곰이 친구들에게 남긴 것은 무엇이었나요?', //기억력
+          category: '기억력',
+          options: [
+            '편지',
+            '코카콜라',
+            '눈사람', //✅
+            '선물 상자'
+          ],
+          answerIndex: 2
+        },
+        {
+          question: '퀴즈3. 친구들 중에서 먼저 눈사람을 쓰다듬은 동물은 누구였나요?', //주의 집중
+          category: '주의 집중력',
+          options: [
+            '여우',
+            '다람쥐', //✅
+            '토끼',
+            '북극곰'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈4. “곰아, 우리랑 같이 놀자!”라는 말에서 친구들의 마음은 어땠을까요?', //언어추론
+          category: '언어 추론력',
+          options: [
+            '곰을 놀리려는 마음이에요.',
+            '곰을 쫓아내려는 마음이에요.',
+            '그냥 심심해서 한 말이에요.',
+            '곰과 진심으로 친구가 되고 싶은 마음이에요.', //✅
+          ],
+          answerIndex: 3
+        }
+      ]
+    },
+        '친구를 지킬거야': {
+      images: [
+        '/friendprotect/friendprotect1.png',
+        '/friendprotect/friendprotect2.png',
+        '/friendprotect/friendprotect3.png',
+        '/friendprotect/friendprotect4.png',
+        '/friendprotect/friendprotect5.png',
+        '/friendprotect/friendprotect6.png',
+        '/friendprotect/friendprotect7.png',
+        '/friendprotect/friendprotect8.png',
+      ],
+      questions: [
+        {
+          question: '퀴즈1. 다람쥐는 왜 곰에게 “그만해!”라고 말했나요?', //이해력
+          category: '이해력',
+          options: [
+            '곰이 미끄럼틀을 먼저 타서',
+            '곰이 토끼를 무섭게 했기 때문이에요', //✅
+            '곰이 다람쥐를 놀렸기 때문이에요',
+            '곰이 장난감을 가져가서'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈2. 곰은 어떤 장난감을 들고 토끼에게 다가갔나요?', //기억력
+          category: '기억력',
+          options: [
+            '나뭇가지',
+            '인형',
+            '공', //✅
+            '당근'
+          ],
+          answerIndex: 2
+        },
+        {
+          question: '퀴즈3. 곰이 토끼에게 공을 던지려 했을 때, 토끼는 어떻게 했나요?', //주의 집중
+          category: '주의 집중력',
+          options: [
+            '뛰어서 달아났어요',
+            '그네에 꼭 붙어 앉았어요', //✅
+            '다람쥐에게 도와달라고 외쳤어요',
+            '곰에게 달려들었어요'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈4. “우리, 서로 기분을 생각하자.”라는 말에서 다람쥐의 마음은 무엇인가요?', //언어추론
+          category: '언어 추론력',
+          options: [
+            '모두가 기분 나빠졌으면 좋겠다는 마음',
+            '더 이상 같이 놀지 말자는 마음',
+            '곰을 혼내주려는 마음',
+            '서로 이해하고 배려하자는 마음', //✅
+          ],
+          answerIndex: 3
+        }
+      ]
+    },
+        '달빛을 나눈 도치': {
+      images: [
+          '/hedgehog/hedgehog1.png',
+          '/hedgehog/hedgehog2.png',
+          '/hedgehog/hedgehog3.png',
+          '/hedgehog/hedgehog4.png',
+          '/hedgehog/hedgehog5.png',
+          '/hedgehog/hedgehog6.png',
+          '/hedgehog/hedgehog7.png',
+          '/hedgehog/hedgehog8.png',
+      ],
+      questions: [
+        {
+          question: '퀴즈1. 왜 달빛 조각을 친구들에게 나누어 주었나요?', //이해력
+          category: '이해력',
+          options: [
+            '친구들이 어두운 밤길을 무서워했기 때문이에요.',
+            '함께 나누면 더 따뜻하고 밝다고 생각했기 때문이에요.', //✅
+            '친구들이 달빛 조각을 훔쳐갔기 때문이에요',
+            '달빛 조각이 너무 무거웠기 때문이에요.'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈2. 달빛 조각을 가장 처음 받은 동물은 누구였나요??', //기억력
+          category: '기억력',
+          options: [
+            '토끼',
+            '새',
+            '부엉이', //✅
+            '다람쥐'
+          ],
+          answerIndex: 2
+        },
+        {
+          question: '퀴즈3. 도치는 달빛 조각을 어디에 담았나요요?', //주의 집중
+          category: '주의 집중력',
+          options: [
+            '상자',
+            '바구니', //✅
+            '주머니',
+            '가방'
+          ],
+          answerIndex: 1
+        },
+        {
+          question: '퀴즈4. “빛은 함께 나눌 때 더 밝아진다”는 말은 어떤 뜻인가요?', //언어추론
+          category: '언어 추론력',
+          options: [
+            '빛은 어두운 곳에서만 사용할 수 있다.',
+            '친구에게 물건을 주면 다시 돌려받아야 한다는 뜻이에요.',
+            '밝은 물건은 금방 사라진다는 뜻이에요.',
+            '혼자보다 친구와 함께 있을 때 기쁨이 커진다는 뜻이에요.', //✅
+          ],
+          answerIndex: 3
+        }
+      ]
+    }
   };
 
   const quiz = quizData[selectedTitle];
